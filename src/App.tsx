c4aa6c5b9f8f6e35faaafa008e896f3ce3d32457
krishmjradiusagent <krish@radiusagent.com>
2026-04-01 18:53:28 +0530

    fix: App.tsx component hierarchy and tsconfig leniency

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import * as React from "react"
 import { Layout } from "./components/Layout"
 import { TeamSettingsContent } from "./modules/TeamSettings/TeamSettingsContent"
-import { GoalSettingSection } from "./modules/GoalSetting/GoalSettingSection"
 import { Toaster } from "sonner"
 
 function App() {
   return (
     <Layout activeTab="Team settings">
       <div className="animate-in fade-in slide-in-from-bottom-4 duration-700">
-        <TeamSettingsContent>
-          <div className="py-4">
-             <GoalSettingSection />
-          </div>
-        </TeamSettingsContent>
+        <TeamSettingsContent />
       </div>
       <Toaster position="bottom-right" richColors />
     </Layout>
