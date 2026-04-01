bf149be3ffbba429ded41679187647477f3558a9
krishmjradiusagent <krish@radiusagent.com>
2026-04-01 18:40:11 +0530

    feat: initial implementation (clean push)

--- packages/shared/dist/constants/metrics.d.ts
+++
@@ -1,5 +0,0 @@
-import { MetricConfig, MetricKey } from '../types.js';
-export declare const METRICS: MetricConfig[];
-export declare const METRIC_KEY_MAP: Record<MetricKey, MetricConfig>;
-export declare const DEFAULT_MONTH: () => string;
-//# sourceMappingURL=metrics.d.ts.map
--- packages/shared/dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from './types.js';
-export * from './constants/metrics.js';
-export * from './services/types.js';
-export { goalService, GoalServiceMock } from './services/mock/goalServiceMock.js';
-export { useGoals } from './hooks/useGoals.js';
-//# sourceMappingURL=index.js.map
